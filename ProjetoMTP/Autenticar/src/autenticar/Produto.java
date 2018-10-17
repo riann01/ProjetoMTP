6e2829415e10357d496df13f9775ac7e25a4004d
Rian <Rian@DESKTOP-AMTDQP4>
2018-10-17 14:05:29 -0300

    Alterações gerais.

--- ProjetoMTP/Autenticar/src/autenticar/Produto.java
+++ ProjetoMTP/Autenticar/src/autenticar/Produto.java
@@ -20,6 +20,8 @@ public class Produto extends javax.swing.JPanel {
         labelNomeProduto = new javax.swing.JLabel();
         labelDescricao = new javax.swing.JLabel();
         labelFotoProduto = new javax.swing.JLabel();
+        labelPreco = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
 
         setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 2, true));
 
@@ -28,12 +30,17 @@ public class Produto extends javax.swing.JPanel {
         labelNomeProduto.setText("NOME DO PRODUTO");
         labelNomeProduto.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
 
-        labelDescricao.setFont(new java.awt.Font("Bahnschrift", 0, 16)); // NOI18N
+        labelDescricao.setFont(new java.awt.Font("Bahnschrift", 0, 14)); // NOI18N
         labelDescricao.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         labelDescricao.setText("<html>\n<body>\n<center>[Nesse campo contém a descrição do produto, ela pode conter características e funções]</center>\n</body>\n</html>");
         labelDescricao.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
 
         labelFotoProduto.setIcon(new javax.swing.ImageIcon(getClass().getResource("/autenticar/Foto/SEM FOTO.png"))); // NOI18N
+
+        labelPreco.setFont(new java.awt.Font("Bahnschrift", 0, 24)); // NOI18N
+        labelPreco.setText("[PREÇO]");
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/autenticar/Foto/adicionar.png"))); // NOI18N
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -43,16 +50,24 @@ public class Produto extends javax.swing.JPanel {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
-                        .addComponent(labelDescricao, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(labelDescricao, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(labelFotoProduto)
+                                .addGap(0, 0, Short.MAX_VALUE))))
                     .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(labelFotoProduto)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(78, 78, 78)
+                                .addComponent(labelNomeProduto))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(152, 152, 152)
+                                .addComponent(jLabel1))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(133, 133, 133)
+                                .addComponent(labelPreco)))
                         .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
-            .addGroup(layout.createSequentialGroup()
-                .addGap(78, 78, 78)
-                .addComponent(labelNomeProduto)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -62,15 +77,21 @@ public class Produto extends javax.swing.JPanel {
                 .addGap(18, 18, 18)
                 .addComponent(labelNomeProduto)
                 .addGap(18, 18, 18)
-                .addComponent(labelDescricao, javax.swing.GroupLayout.DEFAULT_SIZE, 107, Short.MAX_VALUE)
-                .addContainerGap())
+                .addComponent(labelDescricao, javax.swing.GroupLayout.PREFERRED_SIZE, 107, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(13, 13, 13)
+                .addComponent(labelPreco)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel1)
+                .addContainerGap(21, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel labelDescricao;
     private javax.swing.JLabel labelFotoProduto;
     private javax.swing.JLabel labelNomeProduto;
+    private javax.swing.JLabel labelPreco;
     // End of variables declaration//GEN-END:variables
 }
